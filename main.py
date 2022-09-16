97d73cddcec61f8cc57a300869d45ef31c9a4b24
yumekiti <yumekiti120440@gmail.com>
2022-09-16 13:59:06 +0900

    `add` soup

--- main.py
+++ main.py
@@ -14,5 +14,26 @@ options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
 driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', options=options)
 
+# Download the page
+url = sys.argv[1]
+response = requests.get(url)
+html = response.content
+soup = BeautifulSoup(html, 'html.parser')
+
+# body
+body = soup.find('body')
+print(body)
+
+
+driver.get(url)
+driver.implicitly_wait(10)
+html = driver.page_source.encode('utf-8')
+soup = BeautifulSoup(html, 'html.parser')
+
+# body
+body = soup.find('body')
+print(body)
+
 # Done
-print('Done')
+driver.quit()
+print('--- Done ---')
--- readme.md
+++ readme.md
@@ -1,8 +1,14 @@
 # how to use
 
-```
+```sh
 pip install -r requirements.txt
 docker compose up -d
 
 python main.py <url>
+```
+
+# example
+
+```sh
+python main.py https://example.com/
 ```
